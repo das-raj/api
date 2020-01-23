dfbc8cb1d7fc26ba4b94dba77fb539cdf09a57b6
Raj Chandvaniya <rajchandvaniya@gmail.com>
2020-01-23 13:04:10 +0000

    Allowed all hosts

--- api/settings.py
+++ api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'y%c_6wr=n_8#!i24fmdzdnyjx3h8*&o$g)t(quw7363ad=dcd#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
